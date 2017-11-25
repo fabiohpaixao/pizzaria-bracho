975dc74e11b67c0c6ef7a588e34f39cd8a52b7e9
breyller <brunoreyller@gmail.com>
2017-11-25 16:20:19 -0200

    cliente

--- src/br/com/pizzariatreze/model/Cliente.java
+++ src/br/com/pizzariatreze/model/Cliente.java
@@ -1,30 +1,41 @@
 package br.com.pizzariatreze.model;
 
+import br.com.pizzariatreze.dao.Clientedao;
+import br.com.pizzariatreze.dto.Clientedto;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Cliente extends Pessoa {
     
-    public ArrayList<Cliente> getByNome(String nome) {
-        ArrayList<Cliente> result = null;
-        br.com.pizzariatreze.dao.Clientedao clienteDao = new br.com.pizzariatreze.dao.Clientedao();
+    public Object getByNome(String nome) {
+        ArrayList<Clientedto> result = null;
+        Clientedao clienteDao = new Clientedao();
         
         result = clienteDao.getByNome(nome);
         return result;
     }
 
-    public Cliente getById(int id) {
-        Cliente result = null;
-        br.com.pizzariatreze.dao.Clientedao clienteDao = new br.com.pizzariatreze.dao.Clientedao();
+    public Object getById(int id) {
+        Clientedto result = null;
+        Clientedao clienteDao = new Clientedao();
         
         result = clienteDao.getById(id);
         return result;
     }
     
-    public String save() {
+    public String save(Map cliente) {
         String result = null;
-        br.com.pizzariatreze.dao.Clientedao clienteDao = new br.com.pizzariatreze.dao.Clientedao();
+        Clientedao clienteDao = new Clientedao();
+
+        int idCliente = (int)cliente.get("id");
+        String nomeCliente = (String)cliente.get("nome");
+        String enderecoCliente = (String)cliente.get("endereco");
+        String telefoneCliente = (String)cliente.get("telefone");
+        String cpfCliente = (String)cliente.get("cpf");
         
-        result = clienteDao.save(this);
+        Clientedto clienteDto = new Clientedto(idCliente,nomeCliente,enderecoCliente,telefoneCliente,cpfCliente);
+        
+        result = clienteDao.save(clienteDto);
         return result;
     }    
 }
