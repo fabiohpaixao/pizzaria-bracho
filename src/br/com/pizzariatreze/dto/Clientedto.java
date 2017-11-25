87c6f13291f70f02963e11eaea31538b3dbb9e1f
breyller <brunoreyller@gmail.com>
2017-11-25 16:10:40 -0200

    clientedto

--- src/br/com/pizzariatreze/dto/Clientedto.java
+++ src/br/com/pizzariatreze/dto/Clientedto.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pizzariatreze.dto;
 
-/**
- *
- * @author Bracho
- */
 public class Clientedto  extends Pessoadto {
+    
+    public Clientedto(){
+    }
+    
+    public Clientedto(int id, String nome, String endereco, String telefone, String cpf){
+        super(id,nome,endereco,telefone,cpf);
+    }    
     
 }
