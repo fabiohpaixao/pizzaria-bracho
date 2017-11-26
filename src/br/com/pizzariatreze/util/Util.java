eddd5202d9a2ab28e7f56fe904e7baf154f235df
breyller <brunoreyller@gmail.com>
2017-11-26 04:05:48 -0200

    retirar todas essas referencias

--- src/br/com/pizzariatreze/util/Util.java
+++ src/br/com/pizzariatreze/util/Util.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pizzariatreze.util;
 
 import java.io.UnsupportedEncodingException;
@@ -11,10 +6,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Fabio
- */
 public class Util {
     
     public static String criptografar(String texto){
