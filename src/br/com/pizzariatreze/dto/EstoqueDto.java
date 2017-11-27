adc668b062e660dd13cad8594f9af87c8c9788ff
breyller <brunoreyller@gmail.com>
2017-11-27 13:51:45 -0200

    ajustes

--- src/br/com/pizzariatreze/dto/EstoqueDto.java
+++ src/br/com/pizzariatreze/dto/EstoqueDto.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pizzariatreze.dto;
 
-/**
- *
- * @author Fabio
- */
 public class EstoqueDto {
 
     public Object getId() {
