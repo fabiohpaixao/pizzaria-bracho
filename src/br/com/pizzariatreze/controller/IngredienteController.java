94197cbddd282889e52ad35faab297d45cfeadc4
breyller <brunoreyller@gmail.com>
2017-11-27 11:20:52 -0200

    ajustes

--- src/br/com/pizzariatreze/controller/IngredienteController.java
+++ src/br/com/pizzariatreze/controller/IngredienteController.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.pizzariatreze.controller;
 
 import br.com.pizzariatreze.model.Ingrediente;
 import java.util.Map;
 
-/**
- *
- * @author Fabio
- */
 public class IngredienteController {
 
         public boolean save(Map mapIngrediente) {
